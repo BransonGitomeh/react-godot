98d047c8071b21259457a9f83e9c0faf96e1f044
BransonGitomeh <sirbranson67@gmail.com>
2023-12-05 01:47:39 +0300

    test: test service worker pase on netlify

--- postprocess.js
+++ postprocess.js
@@ -9,8 +9,13 @@ async function replace() {
     // Read files
     const html = await fs.readFile(indexPath);
 
+    const scriptsToAdd = `
+        <link rel='manifest' href='index.manifest.json'>
+        <script src="index.service.worker.js"></script>
+        <script src="index.js"></script>
+    `
     // Replace placeholder
-    const replaced = html.toString().replace('<div id="root"></div>', `<div id="root"></div>\n<script src="/index.js"></script>`);
+    const replaced = html.toString().replace('<div id="root"></div>', `<div id="root"></div>\n${scriptsToAdd}`);
 
     // Write back to the HTML file
     await fs.writeFile(indexPath, replaced);
--- static/index.service.worker.js
+++ static/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1701716363|1460472039";
+const CACHE_VERSION = "1701729771|14868110111";
 const CACHE_PREFIX = "Godot-project-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
