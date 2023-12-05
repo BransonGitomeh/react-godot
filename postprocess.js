d3e72815ac231fe05b5d9f1cb8e0be5106c53ac7
BransonGitomeh <sirbranson67@gmail.com>
2023-12-05 03:36:06 +0300

    more styling tests

--- postprocess.js
+++ postprocess.js
@@ -16,7 +16,7 @@ async function replace() {
             display: block;
             position: absolute;
             left: 0;
-            top: auto;
+            top: 60px;
         }
 
         /* Alternatively, using a direct selector for the canvas element */
@@ -24,7 +24,7 @@ async function replace() {
             display: block;
             position: absolute;
             left: 0;
-            top: auto;
+            top: 60px;
         }
         </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
