d15b93664547aadd1e9df5530a7d88a3b8faf7b0
BransonGitomeh <sirbranson67@gmail.com>
2023-12-05 03:31:46 +0300

    more explicit styling

--- postprocess.js
+++ postprocess.js
@@ -10,11 +10,28 @@ async function replace() {
     const html = await fs.readFile(indexPath);
 
     const scriptsToAdd = `
+    <style>
+        /* Using a class for the canvas element */
+        .canvas-style {
+            display: block;
+            position: absolute;
+            left: 0;
+            top: auto;
+        }
+
+        /* Alternatively, using a direct selector for the canvas element */
+        #canvas {
+            display: block;
+            position: absolute;
+            left: 0;
+            top: auto;
+        }
+        </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-      
+
         <link rel='manifest' href='index.manifest.json'>
         <script src="index.js"></script>
     `
