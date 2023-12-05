680ee6fafb8671f44d304eb190ded4068eb6efd4
BransonGitomeh <sirbranson67@gmail.com>
2023-12-05 19:59:42 +0300

    new web builds using ipv6

--- static/index.service.worker.js
+++ static/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1701793088|12292583374";
+const CACHE_VERSION = "1701795479|14683737988";
 const CACHE_PREFIX = "TPS Demo-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
